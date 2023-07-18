d7cd99508c21aad4dd55c8656817462b0bbfbdbd
Nelson Venegas <nelson.venegasg@gmail.com>
2023-07-17 22:22:24 -0400

    feat: new feature navbar with react-astro

---
+++ astro-landing-page/src/assets/images/images.ts
@@ -0,0 +1,6 @@
+const avatarUrl =
+	'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/0a9c9fab-87f0-4659-a31b-e1e930c397c7/deqcjbo-4f7a2bd9-86ce-4d5a-b226-650b9da2c7c8.jpg/v1/fit/w_828,h_1046,q_70,strp/study_02_by_nixeu_deqcjbo-414w-2x.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9MTIwMCIsInBhdGgiOiJcL2ZcLzBhOWM5ZmFiLTg3ZjAtNDY1OS1hMzFiLWUxZTkzMGMzOTdjN1wvZGVxY2piby00ZjdhMmJkOS04NmNlLTRkNWEtYjIyNi02NTBiOWRhMmM3YzguanBnIiwid2lkdGgiOiI8PTk1MCJ9XV0sImF1ZCI6WyJ1cm46c2VydmljZTppbWFnZS5vcGVyYXRpb25zIl19.PAW7JL9_Ro-FlqH1eDA45-v5tlqetvQCYBnAhPX73Qw';
+
+const hola = 'asas';
+
+export { avatarUrl, hola };
---
+++ astro-landing-page/src/hooks/useMediaQuery.ts
@@ -0,0 +1,13 @@
+export function getWindowSize() {
+	let currentWidth = 0;
+	let mediaQuery = false;
+	if (typeof window !== 'undefined') {
+		currentWidth = window.innerWidth;
+	}
+
+	if (currentWidth >= 1280) {
+		mediaQuery = true;
+	}
+
+	return { mediaQuery };
+}
