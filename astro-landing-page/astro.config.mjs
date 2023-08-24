a115d75aa3bdbec499cf59225277b3e8578a5774
Nelson Venegas <nelson.venegasg@gmail.com>
2023-08-23 23:51:13 -0400

    fix: new config to deploy correctly

--- astro-landing-page/astro.config.mjs
+++ astro-landing-page/astro.config.mjs
@@ -1,11 +1,14 @@
+import react from '@astrojs/react'
 import tailwind from '@astrojs/tailwind'
 import { defineConfig } from 'astro/config'
 
-import react from '@astrojs/react'
+import vercel from '@astrojs/vercel/serverless'
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
+  adapter: vercel(),
+  output: 'server',
   experimental: {
     viewTransitions: true
   }
