9ab2b74be555a539412b7cbb4a2cfb397dc49db0
Nelson Venegas <nelson.venegasg@gmail.com>
2023-08-23 23:11:40 -0400

    fix: modified astro config

--- astro-landing-page/astro.config.mjs
+++ astro-landing-page/astro.config.mjs
@@ -6,7 +6,6 @@ import react from '@astrojs/react'
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
-  output: 'server',
   experimental: {
     viewTransitions: true
   }
